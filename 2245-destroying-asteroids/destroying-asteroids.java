3972fe5276d7c701dd451707bcded41b6c3c776d
Debadipta Sahu <debssahu123@gmail.com>
2026-05-31 15:00:51 +0530

    Time: 26 ms (81.52%) | Memory: 107.6 MB (30.95%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m = mass;
+        for(int num : asteroids){
+            if(num <= m){
+                m += num;
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
