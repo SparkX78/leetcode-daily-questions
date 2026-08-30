45349b02b8ed9cb7362d6245c4f576b53809f02e
Debadipta Sahu <debssahu123@gmail.com>
2026-08-30 12:30:25 +0530

    Time: 15 ms (26.53%) | Memory: 48.8 MB (76.52%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,37 +1,55 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<int[]>[] graph = new ArrayList[n+1];
-        for(int i = 1; i <= n; i++){
-            graph[i] = new ArrayList<>();
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            adj.add(new ArrayList<>());
         }
-        for(int[] t: times){
-            graph[t[0]].add(new int[]{t[1], t[2]});
+        for(int[] edge : times){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(v);
+            temp.add(w);
+
+            adj.get(u).add(temp);
         }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
         int[] dist = new int[n+1];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-a[1]);
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
-        pq.add(new int[] {k,0});
+        pq.offer(new int[]{0, k});
+
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
-            int node = curr[0];
-            int d = curr[1];
-            for(int[] neighbour : graph[node]){
-                int next = neighbour[0];
-                int weight = neighbour[1];
-                if(d + weight < dist[next]){
-                    dist[next] = d+weight;
-                    pq.add(new int[]{next, dist[next]});
+            int D = curr[0];
+            int node = curr[1];
+
+            if(D > dist[node]){
+                continue;
+            }
+
+            for(ArrayList<Integer> neigh : adj.get(node)){
+                int next_node = neigh.get(0);
+                int weight = neigh.get(1);
+
+                if(weight + D < dist[next_node]){
+                    dist[next_node] = weight + D;
+                    pq.offer(new int[]{dist[next_node] ,next_node});
                 }
             }
+
         }
-        int ans = 0;
+        int maxTime = 0;
         for(int i = 1; i <=n; i++){
             if(dist[i] == Integer.MAX_VALUE){
                 return -1;
             }
-            ans = Math.max(ans, dist[i]);
+
+            maxTime = Math.max(maxTime, dist[i]);
         }
-        return ans;
+        return maxTime;
     }
 }
