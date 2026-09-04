1680cde900b486bfc73aee8fdf40e700d259d2c5
Debadipta Sahu <debssahu123@gmail.com>
2026-09-04 20:06:49 +0530

    Time: 253 ms (19.73%) | Memory: 161.3 MB (5.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,37 @@
 class Solution {
+    Boolean[][] dp ;
     public String longestPalindrome(String s) {
+        int n = s.length();
+        dp = new Boolean[n+1][n+1];
+        
         int maxLen = Integer.MIN_VALUE;
-        int sp = 0;
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                if(solve(s, i, j)){
+        int start = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(solve(s,i, j) == true){
                     if(j-i+1 > maxLen){
                         maxLen = j-i+1;
-                        sp = i;
+                        start = i;
                     }
                 }
+                
             }
         }
-        return s.substring(sp, sp+ maxLen);
+        return s.substring(start, start + maxLen);
     }
-    private boolean solve(String s, int i, int j){
+    public boolean solve(String s, int i, int j){
         if(i >= j){
             return true;
         }
-        if(s.charAt(i) == s.charAt(j)){
-            return solve(s, i+1, j-1);
+        if(dp[i][j] != null){
+            return dp[i][j];
         }
-        return false;
+        if(s.charAt(i) == s.charAt(j)){
+            dp[i][j] = solve(s, i+1, j-1);
+        }
+        else{
+            dp[i][j] = false;
+        }
+        return dp[i][j];
     }
 }
